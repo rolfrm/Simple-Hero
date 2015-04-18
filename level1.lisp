e8173cd154e1cf314c5c8fb5a1c05223a258b8a2
Rolf Madsen <rolfrm@gmail.com>
2015-04-18 11:39:30 +0200

    added level1 spec

---
+++ level1.lisp
@@ -0,0 +1,11 @@
+
+(entity :id "player"
+ (circle 0.0 0.0 1.0))
+
+(entity :id "enemy"
+	(color (from-rgb 255 0 0)
+	       (circle 0.0 0.0 0.1)))
+
+(scenery :id "world"
+  (sub (circle 0.0 0.0 10.0)
+       (circle 0.0 0.0 8.0)))
